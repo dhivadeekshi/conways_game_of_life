06812bd212a53ce4cd7dd046bb3adf662776150b
Dhivakar Subramaniam <subramaniamdhivakar@gmail.com>
2019-10-16 23:21:58 +0530

    Implemented PoolManager

---
+++ Assets/Scripts/PoolManager/PoolContainer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolContainer : MonoBehaviour {
+    private string poolName = string.Empty;
+    private GameObject poolItemPrefab = null;
+    private List<PoolItem> poolItems = new List<PoolItem>();
+
+    public void InitPoolFor(string poolName, GameObject poolItemPrefab, int noOfItems)
+    {
+        this.poolName = poolName;
+        this.poolItemPrefab = poolItemPrefab;
+        CreatePoolItems(noOfItems);
+    }
+
+    public void CreatePoolItems(int noOfItems)
+    {
+        GameObject poolInstance;
+        PoolItem poolItem;
+        for (int i = 0; i < noOfItems; i++)
+        {
+            poolInstance = Instantiate(poolItemPrefab, transform);
+            poolInstance.SetActive(false);
+            poolItem = poolInstance.AddComponent<PoolItem>();
+            poolItem.PoolName = poolName;
+            poolItems.Add(poolItem);
+        }
+    }
+
+    public bool IsPoolOf(string poolName) { return this.poolName.Equals(poolName); }
+
+    public GameObject GetItemFromPool()
+    {
+        if (poolItems.Count == 0)
+            CreatePoolItems(1);
+        
+        PoolItem poolItem = poolItems[0];
+        poolItems.RemoveAt(0);
+
+        return poolItem.gameObject;
+    }
+
+    public void ReturnItemToPool(PoolItem poolItem)
+    {
+        poolItem.gameObject.transform.SetParent(transform);
+        poolItem.gameObject.SetActive(false);
+        poolItem.transform.position = Vector3.zero;
+        poolItems.Add(poolItem);
+    }
+}
---
+++ Assets/Scripts/PoolManager/PoolManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour {
+
+    private const int INITIAL_POOL_COUNT = 3;
+    private static PoolManager instance = null;
+    public static PoolManager Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                GameObject poolManagerObject = new GameObject("PoolManager");
+                instance = poolManagerObject.AddComponent<PoolManager>();
+            }
+            return instance;
+        }
+    }
+
+    private List<PoolContainer> poolContainers = new List<PoolContainer>();
+
+    public void CreatePoolFor(string poolName, GameObject poolItemPrefab, int poolCount = INITIAL_POOL_COUNT)
+    {
+        GameObject gameObject = new GameObject("PoolFor" + poolName);
+        gameObject.transform.SetParent(transform);
+        gameObject.AddComponent<PoolContainer>().InitPoolFor(poolName, poolItemPrefab, poolCount);
+        poolContainers.Add(gameObject.GetComponent<PoolContainer>());
+    }
+
+    public GameObject GetItemFromPool(string poolName)
+    {
+        PoolContainer poolContainer = poolContainers.Find((x) => { return x.IsPoolOf(poolName); });
+        return poolContainer.GetItemFromPool();
+    }
+
+    public void ReturnItemToPool(PoolItem poolItem)
+    {
+        PoolContainer poolContainer = poolContainers.Find((x) => { return x.IsPoolOf(poolItem.PoolName); });
+        poolContainer.ReturnItemToPool(poolItem);
+    }
+}
