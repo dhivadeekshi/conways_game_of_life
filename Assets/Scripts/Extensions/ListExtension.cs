b6ba1966aae27420d2ebf2604c3c2fd4800951a8
Dhivakar Subramaniam <subramaniamdhivakar@gmail.com>
2019-10-17 17:49:37 +0530

    Added extension to List to print all elements

---
+++ Assets/Scripts/Extensions/ListExtension.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class ListExtension {
+
+    public static void PrintAllElements<T>(this List<T> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+            Debug.Log("Element at " + i + ": " + list[i]);
+    }
+}
