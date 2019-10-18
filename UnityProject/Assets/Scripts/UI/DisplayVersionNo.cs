8eb595145e5819b9bbe990258f40483d5baba640
Dhivakar Subramaniam <subramaniamdhivakar@gmail.com>
2019-10-19 00:18:16 +0530

    Added version no display to the bottom right corner of the screen

---
+++ UnityProject/Assets/Scripts/UI/DisplayVersionNo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class DisplayVersionNo : MonoBehaviour {
+
+    void Start()
+    {
+        GetComponent<Text>().text = "v " + Application.version;
+    }
+}
